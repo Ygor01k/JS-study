ad30887326e7aebea92df23a52c8b69743f2cbc3
Ygor01k <81721426+Ygor01k@users.noreply.github.com>
2022-12-21 17:45:38 -0300

    Adjusting paths and exercise 37 done

---
+++ exercises/37 Max of two numbers/index.js
@@ -0,0 +1,25 @@
+//Exercise 37 Max of two numbers
+
+// my code
+let a = 15;
+let b = 50;
+
+// Method 1
+ if (a > b)
+    console.log(a);
+ else
+    console.log(b);
+  
+ // Method 2
+ let MaxNumber;
+ if (a > b)
+    MaxNumber = a;
+ else
+    MaxNumber = b;
+  
+ console.log(MaxNumber);
+
+
+// Instructor's code
+
+
---
+++ exercises/43 string propertis/index.js
@@ -0,0 +1,28 @@
+//Exercise 27.12 Swapping variables
+
+// my code
+let a = 'red';
+let b = 'blue';
+
+// Simple form
+ let c = b;
+ b = a;
+ a = c;
+
+
+console.log(a);
+console.log(b);
+
+
+// Instructor's code
+
+// let a = 'red';
+// let b = 'blue';
+
+//  let c = a;
+//  a = b;
+//  b = c;
+
+
+// console.log(a);
+// console.log(b);
